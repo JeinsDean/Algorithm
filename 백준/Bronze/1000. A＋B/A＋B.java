dd2f72aea3ec1e5cdeda99b5dcefc8b3a8ff63c0
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-01-03 15:12:20 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b = 0;
+
+		do {
+			a = sc.nextInt();
+			b = sc.nextInt();
+		} while ((10 <= a || a <= 0) || (10 <= b || b <= 0));
+
+		System.out.println(a + b);
+	}
+}
