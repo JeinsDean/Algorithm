5731e83cc0cc0a7d41d49740ea5ca292deaef19c
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-01-19 17:49:51 +0900

    [Bronze III] Title: 알람 시계, Time: 148 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int h = sc.nextInt();
-		int m = sc.nextInt();
-		sc.close();
+		String[] str = br.readLine().split(" ");
+		br.close();
+		int h = Integer.parseInt(str[0]);
+		int m = Integer.parseInt(str[1]);
 
 		if (h == 0 && m < 45) {
 			h = 23;
