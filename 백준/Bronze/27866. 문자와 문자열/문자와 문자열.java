058b428dd3a8cff24f5921e0ff3b03da3558593a
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-02-03 23:13:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 128 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String str = br.readLine();
+                int idx = Integer.parseInt(br.readLine());
+
+                br.close();
+                char[] chr = str.toCharArray();
+                System.out.println(chr[idx - 1]);
+        }
+}
