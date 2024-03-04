72c5977b9a2458e681cf7768d36f01e81ade002c
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-03-04 16:55:39 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 136 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,21 +1,24 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] student = new int[31];
-		
-		for(int i=1; i<29; i++) {
-			int success = sc.nextInt();
-			student[success] = 1;
-		}
-		for(int i=1; i<student.length; i++) {
-			if(student[i]!=1)
-				System.out.println(i);
-		}
-		
-		
-		sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 1; i <= 30; i ++) {
+            list.add(i);
+        }
 
-	}
+        for(int i = 1; i <= 28; i++) {
+            list.remove((Object)Integer.parseInt(br.readLine()));
+        }
+        Arrays.sort(list.toArray());
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
 }
