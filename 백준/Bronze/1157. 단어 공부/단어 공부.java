2daeb5f5779e30c24f45b174eab188af354a9f15
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-03-06 13:04:23 +0900

    [Bronze I] Title: 단어 공부, Time: 572 ms, Memory: 64396 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toUpperCase();
+        Set<Character> set = new HashSet<Character>();
+        int cnt = 0;
+        String ans = "";
+        
+        for (char c : str.toCharArray()) {
+            set.add(c);
+        }
+
+        for (Character c : set) {
+            int tmp = str.length() - str.replace(c.toString(), "").length();
+            if(cnt == tmp) {
+                ans = "?";
+            } else {
+                if(cnt < tmp) {
+                    cnt = tmp;
+                    ans = c.toString();    
+                }
+            }
+        }
+        System.out.println(ans);        
+    }
+}
