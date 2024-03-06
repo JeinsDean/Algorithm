58ce3c06d6a1eecca6db50bab6abeb173e33fbec
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-03-06 13:13:13 +0900

    [Bronze I] Title: 단어 공부, Time: 288 ms, Memory: 21044 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,30 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine().toUpperCase();
-        Set<Character> set = new HashSet<Character>();
-        int cnt = 0;
-        String ans = "";
-        
-        for (char c : str.toCharArray()) {
-            set.add(c);
+        int[] arr = new int[26];
+        int max = -1;
+        char ch = '?';
+
+        for(int i = 0; i < str.length(); i++) {
+            arr[str.charAt(i) - 'A']++;
         }
 
-        for (Character c : set) {
-            int tmp = str.length() - str.replace(c.toString(), "").length();
-            if(cnt == tmp) {
-                ans = "?";
-            } else if(cnt < tmp) {
-                cnt = tmp;
-                ans = c.toString();    
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                ch = (char)(i + 65);
+            } else if(arr[i] == max) {
+                ch = '?';
             }
         }
-        System.out.println(ans);        
+        System.out.println(ch);
+
     }
 }
