fea9b2ca4966856d08023d4626836b32e65d9de4
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-04-01 10:10:44 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 116 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,25 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());  //테스트 케이스 개수
-        int quarter = 25;  //쿼터 0.25$
-        int dime = 10;  //다임 0.10$
-        int nickel = 5;  //니켈 0.05$
-        int penny = 1;  //페니 0.01$
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            int c = Integer.parseInt(br.readLine());
-            sb.append(c / quarter + " ");
-            c %= quarter;
-            sb.append(c / dime + " ");
-            c %= dime;
-            sb.append(c / nickel + " ");
-            c %= nickel;
-            sb.append(c / penny + "\n");
+
+        for(int i = 0; i < T; i++) {
+            int p = Integer.parseInt(br.readLine());
+            int q = p / 25;
+            p = p - (q * 25);
+
+            int d = p / 10;
+            p = p - (d * 10);
+
+            int n = p / 5;
+            p = p - (n * 5); // 나머지 금액은 1원
+            
+            sb.append(q).append(" ").append(d).append(" ").append(n).append(" ").append(p).append("\n");
         }
-        System.out.print(sb);
+        br.close();
+        System.out.println(sb);
     }
-}
+}   
