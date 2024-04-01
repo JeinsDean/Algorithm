8e8fd51e84f559edafafac57cf2b8ede8cebbcd7
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-04-01 17:05:49 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,26 +1,24 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
-		int cnt = 1;
-		int rng = 2;
- 
-		if (N == 1) {
-			System.out.print(1);
-		}
- 
-		else {
-			while (rng <= N) {
-				rng = rng + (6 * cnt);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        int cnt = 1;
+        int sum = 2;
+
+        if(N == 1) {
+            System.out.println(1);
+            
+        } else {
+            while (sum <= N) {
+				sum = sum + (6 * cnt);
 				cnt++;
 			}
 			System.out.print(cnt);
-		}
-	}
-}
+        }
+    }
+}   
