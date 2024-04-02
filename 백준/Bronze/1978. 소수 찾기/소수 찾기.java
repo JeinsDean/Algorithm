bc4a46c33713f7cd82caa26f5d9aa71616d22247
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-04-02 14:03:09 +0900

    [Bronze II] Title: 소수 찾기, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,35 +1,32 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		int count = 0;
-		
-		for(int i = 0; i < N; i++) {
-			
-			// 소수인경우 true, 아닌경우 false   
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int cnt = 0;
+		while(st.hasMoreTokens()) {
 			boolean isPrime = true;
+			int n = Integer.parseInt(st.nextToken());
 			
-			int num = in.nextInt();
-			
-			if(num == 1) {	// 1 인경우 다음 반복문으로
+			if(n == 1) {
 				continue;
 			}
-			for(int j = 2; j <= Math.sqrt(num); j++) {
-				if(num % j == 0) {
-					isPrime = false;	// 소수가 아니므로 false 로 바꿔줌
+			for(int i = 2; i <= Math.sqrt(n); i++) {
+				if(n % i == 0) {
+					isPrime = false;
 					break;
 				}
 			}
-			if(isPrime) {	// 소수인경우 count 값 1 증가
-				count++;
+			if(isPrime) {
+				cnt++;
 			}
 		}
-		System.out.println(count);
+		System.out.println(cnt);
 	}
- 
 }
