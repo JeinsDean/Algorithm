d2cc63ccc7c4d69368587543891289346bce1daa
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-04-06 23:53:05 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[str.length()];
+        br.close();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Character.getNumericValue(str.charAt(i));
+        }
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
