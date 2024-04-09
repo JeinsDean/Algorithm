029f36364111a40cc4915c0f22a6444cf4e538ec
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-04-09 10:18:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 152 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -8,16 +8,18 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         br.close();
 
-        int cnt = 0; 
         int idx = 666;
         // 666, 1666, 2666, 3666, 4666, 5666, 6666, 7666, 8666, 9666, 10666, 11666
-        while (cnt < N) {
-            if(String.valueOf(idx).contains("666")) {
-                cnt++;
+        while (N > 0) {
+            int temp = idx;
+            while (temp >= 666) {
+                if (temp % 1000 == 666) {
+                    N--;
+                    break;
+                } else temp /= 10;
             }
             idx++;
         }
-        idx = idx - 1;
-        System.out.println(idx);
+        System.out.println(--idx);
     }
 }
