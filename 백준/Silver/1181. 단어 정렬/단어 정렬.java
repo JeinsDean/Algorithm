06cdff6503e1cdf61c2882a8a22cb84cb2bd0825
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-04-11 10:57:05 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 24696 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,40 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        List<String> list = new ArrayList<String>();
-
-        for(int i = 0; i < N; i++) {
-            String str = br.readLine();
-            if(list.contains(str)) {
-                continue;
+        Set<String> set = new TreeSet<String>(new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                if(a.length() != b.length()) {
+                    return a.length() - b.length();
+                } else {
+                    return a.compareTo(b);
+                }
             }
-            list.add(str);
+        });
+        for(int i = 0; i < N; i++) {
+            set.add(br.readLine());
         }
         br.close();
 
-        String[] arr = list.toArray(new String[list.size()]);
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return o1.length() - o2.length();
-            }
-        });
-        
-        for (String str : arr) {
-            sb.append(str).append("\n");
+        for(String s : set){
+            sb.append(s).append("\n");
         }
         System.out.println(sb);
     }
