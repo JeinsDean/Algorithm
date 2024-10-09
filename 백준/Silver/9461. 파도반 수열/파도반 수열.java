95178b340ec5cb6db3fafc08ca325cf06f4d3062
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2024-10-09 23:52:19 +0900

    [Silver III] Title: 파도반 수열, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static Long[] seq = new Long[101];
+ 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		seq[0] = 0L;
+		seq[1] = 1L;
+		seq[2] = 1L;
+		seq[3] = 1L;
+		
+		int T = sc.nextInt();
+		
+		while(T-- > 0) {
+			int N = sc.nextInt();
+			System.out.println(padovan(N));
+		}
+		sc.close();
+	}
+	
+	public static long padovan(int N) {
+		if(seq[N] == null) {
+			seq[N] = padovan(N - 2) + padovan(N - 3);
+		}
+		return seq[N];
+	}
+ 
+}
