8f66a5be0546b5bbc6ebc148292950ee3892f09c
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2025-07-31 22:53:15 +0900

    [Bronze V] Title: A+B - 5, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a, b;
-		while (true) {
-			String str = br.readLine();
-			StringTokenizer st = new StringTokenizer(str, " ");
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			if (a == 0 && b == 0) {
-				break;
-			}
-			System.out.println(a + b);
-		}
-		br.close();
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input;
+
+        do {
+            input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            if(a != 0 && b != 0) {
+                System.out.println(a + b);
+            }
+        }  while (!Arrays.stream(input).allMatch(s -> s.equals("0")));
+
+        br.close();
+    }
 }
