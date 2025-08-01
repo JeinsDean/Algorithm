c81735ba4d9a92ed4ec6d61b823596420050a5f5
JeinsDean <154984766+JeinsDean@users.noreply.github.com>
2025-08-01 17:59:52 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 16444 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,28 @@
+import java.util.Arrays;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] arr = new int[Integer.parseInt(br.readLine())];
-        int max = 0;
-        float avg = 0;
-        String[] tmp = br.readLine().split(" ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(tmp[i]);
-            max = max > arr[i] ? max : arr[i];
-        }
-
-        for (int i : arr) {
-            avg += (float)i / max * 100;
-        }
-        avg = avg / arr.length;
-
-        System.out.println(avg);
-    }
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		double arr[] = new double[Integer.parseInt(br.readLine())];
+        
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = Double.parseDouble(st.nextToken());
+		}
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += ((arr[i] / arr[arr.length - 1]) * 100);
+		}
+		System.out.print(sum / arr.length);
+	}
 }
